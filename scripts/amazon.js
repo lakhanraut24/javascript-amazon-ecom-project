c3cccff7a3e5d894550734039548e110a5b2735d
LakhanRaut <lakhanraut24@gmail.com>
2023-09-21 23:28:01 +0530

    generating html in javascript

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,5 @@
-/*   this is the data structure : because it structure or organize the data(information about product)
- and this represents a list of products, so we usually use a combination of objects and arrays to create a data-structure in javascript*/
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: " Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCent: 1090 /* we will save it as 1090 as reminder, $1=100 cents*/,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4.0,
-      count: 127,
-    },
-    priceCent: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: " Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCent: 799,
-  }];
+
+
 
   let productsHTML = '';
 
@@ -52,7 +24,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-price">
-     $${(product.priceCent / 100).toFixed(2)}
+     $${(product.priceCents / 100).toFixed(2)}
     </div>
 
     <div class="product-quantity-container">
