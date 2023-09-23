6f12b4241ac50c470ec4066a9151721c8a0c561c
LakhanRaut <lakhanraut24@gmail.com>
2023-09-23 12:28:37 +0530

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
         Delivery date: Tuesday, June 21
         </div>
@@ -104,5 +104,7 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
     
-  });
+   const container = document.querySelector(`.js-cart-item-container-${productId}`)
+  container.remove();
+});
 });
